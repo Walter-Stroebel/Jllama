06b32a5d03998aeb73f17e7b4ddad2e34d403716
Walter Stroebel <info@infcomtec.nl>
2024-03-24 12:40:33 +0100

    Saved

--- src/main/java/nl/infcomtec/simpleimage/DisplayMonitor.java
+++ src/main/java/nl/infcomtec/simpleimage/DisplayMonitor.java
@@ -1,5 +1,3 @@
-/*
- */
 package nl.infcomtec.simpleimage;
 
 import java.awt.DisplayMode;
@@ -8,6 +6,9 @@ import java.awt.GraphicsEnvironment;
 import java.awt.Rectangle;
 
 /**
+ * Simple utility class to get information about the system's monitor(s).
+ *
+ * The getter functions are optional as the members are public and final.
  *
  * @author walter
  */
@@ -16,6 +17,10 @@ public class DisplayMonitor {
     public final GraphicsEnvironment grEnv = GraphicsEnvironment.getLocalGraphicsEnvironment();
     public final GraphicsDevice defaultScreen = grEnv.getDefaultScreenDevice();
     public final DisplayMode displayMode = defaultScreen.getDisplayMode();
+
+    public GraphicsEnvironment getGraphicsEnvironment() {
+        return grEnv;
+    }
 
     public Rectangle getDefaultBounds() {
         return defaultScreen.getDefaultConfiguration().getBounds();
